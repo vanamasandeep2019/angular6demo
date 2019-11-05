9756e8074f7c8a6d4689f2eec6c5a247bcc62e3b
krishna sandeep <vanamasandeep2019@gmail.com>
2019-11-05 12:05:25 +0530

    Commit All files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/date/date.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'date',
+  templateUrl: './date.component.html',
+  styleUrls: ['./date.component.css']
+})
+export class DateComponent implements OnInit {
+
+  dateMessage:string;
+
+  constructor() { 
+    setInterval(()=>{
+      let currentDate=new Date();
+    this.dateMessage=currentDate.toDateString()+' '+currentDate.toLocaleTimeString() 
+
+    },1000);  
+       
+  }
+
+  ngOnInit() {
+  }
+
+}
